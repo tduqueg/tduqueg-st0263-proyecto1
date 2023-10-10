9dc3f937563aff6b30922f9a5eec87476df03a4d
tduqueg <tduqueg@eafit.edu.co>
2023-10-10 14:43:12 -0500

    Naming Node

--- source/nodeName/server.js
+++ source/nodeName/server.js
@@ -1,1 +1,28 @@
-const 
+const express = require("express");
+const bodyParser = require("body-parser");
+const findController = require("./controllers/findController");
+const systemController = require("./controllers/systemController");
+
+function server(port, host, config) {
+  const app = express();
+  app.use(bodyParser.json());
+
+  app.use((req, res, next) => {
+    req.config = config;
+    req.storageMap = storageMap;
+    next();
+  });
+
+  app.get("/find", findController.find);
+  app.get("/resource", resourceController.create);
+  app.put("/resource", resourceController.update);
+  app.get("/health", systemController.health);
+  app.get("/hashmap", systemController.hashmap);
+  app.get("/ls", systemController.ls);
+
+  app.listen(port, () => {
+    console.log(`El nodo naming en  http://${host}:${port}`);
+  });
+}
+
+module.exports = { server };
---
+++ source/nodeName/startup.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+const storage = require("../utils/storage");
+const e = require("express");
+
+function startup(config) {
+  console.log("Empezando...");
+  const storageNodes = config.storageNodes;
+  console.log("Nodos de almacenamiento: ", storageNodes);
+
+  const otherNameNodeIP = config.namingNodes[0];
+  console.log("Otro nodo naming: ", otherNameNodeIP);
+
+  axios
+    .get(`http://${otherNameNodeIP}:80/health`)
+    .then((response) => {
+      if (response.status === 200) {
+        axios
+          .get(`http://${otherNameNodeIP}:80/hashmap`)
+          .then((hashMapResponse) => {
+            storage.setEntireMap(hashMapResponse.data);
+            console.log("HashMap sincronizado con el otro nodo");
+          })
+          .catch((error) => {
+            console.error(
+              "Error al sincronizar el HashMap con el otro nodo",
+              error
+            );
+          });
+      }
+    })
+    .catch((error) => {
+      //HOla como estás
+    });
+}
+
+module.exports = { startup };
