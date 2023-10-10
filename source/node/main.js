f5e76c716c9e1825c1cf96e05b60fc292cfc02f6
tduqueg <tduqueg@eafit.edu.co>
2023-10-10 12:32:24 -0500

    El main de los nodos :D

---
+++ source/node/main.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+const path = require("path");
+const fs = require("fs").promises;
+const { server } = require("./server");
+const { grpc } = require("./grpc");
+
+dotenv.config({ path: path.join(__dirname, "../.env") });
+const configPath = path.join(__dirname, "./config.json");
+const host = process.env.STORAGE_HOST;
+const port = process.env.STORAGE_PORT;
+
+async function main() {
+  const configContent = await fs.readFile(configPath, "utf8");
+  const config = JSON.parse(configContent);
+
+  startup(config || {});
+  server(port, host, config);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
